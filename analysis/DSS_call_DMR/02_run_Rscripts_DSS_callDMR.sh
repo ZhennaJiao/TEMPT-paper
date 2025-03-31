024f090ba3eb0385cee7f559438ee269cad0a12a
Zhenna Jiao <149151196+Loopy-fox@users.noreply.github.com>
2025-03-31 16:02:39 +0800

    Update 02_run_Rscripts_DSS_callDMR.sh

--- analysis/DSS_call_DMR/02_run_Rscripts_DSS_callDMR.sh
+++ analysis/DSS_call_DMR/02_run_Rscripts_DSS_callDMR.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=callDMR_filter_add_sample
-#SBATCH --partition fat
-#SBATCH -o /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_filter_add_sample_20250213/logs/%j_%x.log
-#SBATCH -e /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_filter_add_sample_20250213/logs/%j_%x.log
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=2023206500042@whu.edu.cn
-#SBATCH --mem-per-cpu=25G
-#SBATCH -n 8
-
-## modified 20250109 
-# fscript_callDMR=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_20250107/codes/01_DSS_callDMR_14_sample_20250109.r
-
-
-#  /home/software/anaconda3/bin/Rscript $fscript_callDMR
-
+# fscript_callDMR=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_20250107/codes/01_DSS_callDMR_14_sample_20250109.r
 
  ## modified 20250114 by Zhenna Jiao
- fscript_callDMR=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_filter_add_sample_20250213/codes/01_1_DSS_callDMR_filter_added_sample_20250211.r
+ fscript_callDMR=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/Call_DMR_filter_add_sample_20250213/codes/01_1_DSS_callDMR_filter_added_sample_20250211.r
 
  /home/software/anaconda3/bin/Rscript $fscript_callDMR
