defe025025b6d1dd644f9d8d3810f101015b3557
Zhenna Jiao <149151196+Loopy-fox@users.noreply.github.com>
2025-03-31 15:51:59 +0800

    Update 02_data_trim.sh

--- preprocessing/02_data_trim.sh
+++ preprocessing/02_data_trim.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=trim
-#SBATCH --partition cpu
-#SBATCH -o /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/logs/%j_%x.log
-#SBATCH -e /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/logs/%j_%x.log
-#SBATCH --nodes=1
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=2023206500042@whu.edu.cn
-#SBATCH -n 4
-
-
 ## Zhenna Jiao
 ## 25 NOV 2024 modified 
-
 
 module load trim_galore/0.6.10
 module load fastqc/0.12.1   
 
-data_proj=/home/jiaozhenna/methylation/project/EM_seq/data/20241125_TEMPT_RESEQ_DATA/sample_1x_data_20241125/241121-A00151B
+data_proj=$HOME/methylation/project/EM_seq/data/20241125_TEMPT_RESEQ_DATA/sample_1x_data_20241125/241121-A00151B
 
-output=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/02_trimred_data_20241125
+output=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/02_trimred_data_20241125
 
 sample_list=("L15-1" "L15-2" "L15-3" "L15-4" "L15-5" "L15-6" "L15-7" "L15-8")
 
