234605643840cb092c7df6dbcd6427b0c332727a
Zhenna Jiao <149151196+Loopy-fox@users.noreply.github.com>
2025-03-31 15:53:01 +0800

    Update 03_data_bismap.sh

--- preprocessing/03_data_bismap.sh
+++ preprocessing/03_data_bismap.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=map
-#SBATCH --partition fat
-#SBATCH -o /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/logs/%j_%x.log
-#SBATCH -e /home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/logs/%j_%x.log
-#SBATCH --nodes=1
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=2023206500042@whu.edu.cn
-#SBATCH --mem-per-cpu=25G
-#SBATCH -n 4
-
-
 ##Zhenna Jiao
 ## 30 April 24 modified
 
 eval "$(conda shell.bash hook)"
-conda activate /home/jiaozhenna/.conda/envs/meth
+conda activate $HOME/.conda/envs/meth
 
 sample_list=("L15-1" "L15-2" "L15-3" "L15-4" "L15-5" "L15-6" "L15-7" "L15-8")
 
 
-ref=/home/jiaozhenna/methylation/ref/bis_ref/
+ref=$HOME/methylation/ref/bis_ref/
 
-out_dir=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/03_mapped_data_20241125/
+out_dir=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/03_mapped_data_20241125/
 
-ptemp_dir=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/tmp
+ptemp_dir=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/tmp
 
-input_dir=/home/jiaozhenna/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/02_trimred_data_20241125
+input_dir=$HOME/methylation/project/EM_seq/20241125_data_reanalysis/reprocessing/results/02_trimred_data_20241125
 
-for smaple in ${sample_list[@]}
+for sample in ${sample_list[@]}
 do
-    fq1=${input_dir}/${smaple}_L2_1_val_1.fq.gz
-    fq2=${input_dir}/${smaple}_L2_2_val_2.fq.gz
+    fq1=${input_dir}/${sample}_L2_1_val_1.fq.gz
+    fq2=${input_dir}/${sample}_L2_2_val_2.fq.gz
     
 
         bismark --parallel 4  --genome_folder ${ref} \
